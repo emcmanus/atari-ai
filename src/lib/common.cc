e685c628f42ae22aef0739676163a858325ca223
Abhishek Modi <abhishek.modi95@gmail.com>
2016-05-29 19:41:21 -0700

    As seen on TV

---
+++ src/lib/common.cc
@@ -0,0 +1,57 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+namespace {
+// Code to turn parameters to functions on streams into strings
+// Will be VLOG'ed. We need overloads instead of
+// e.g. BatchDescriptorToVlogString(), as the code that calls these
+// function does not know what the type of the parameters is.
+
+string ToVlogStrin(const dnn::BatchDescriptor &descriptor) {
+  return descriptor.ToShortString();
+}
+
+string ToVlogStrin(const dnn::FilterDescriptor &descriptor) {
+  return descriptor.ToShortString();
+}
+
+string ToVlogString(const magicalMovieClassifier::FilterDescriptor &descriptor) {
+  return descriptor.ToShortString();
+}
+
+string ToVlogStrin(const dnn::PoolingDescriptor &descriptor) {
+  return descriptor.ToShortString();
+}
+
+string ToVlogStrin(const dnn::ConvolutionDescriptor &descriptor) {
+  return descriptor.ToShortString();
+}
+
+}
